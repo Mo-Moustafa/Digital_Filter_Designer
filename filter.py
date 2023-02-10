39e46423c0c1c90d745b8b0b2522386d5199beeb
Mo-Moustafa <momoustafa227@gmail.com>
2023-02-10 04:12:51 +0200

    all pass done

--- filter.py
+++ filter.py
@@ -10,13 +10,16 @@ from flask import redirect, url_for
 
 app = Flask(__name__)
 
+input_signal = []
+
 combined_poles = []
 combined_zeros = []
-input_signal = []
-allPassZeros=[]
-allPassPoles=[]
-allpass=[]
-allpassConj=[]
+
+allPassZeros = []
+allPassPoles = []
+
+appliedAPFZeros = []
+appliedAPFPoles = []
 
 @app.route('/')
 def home():
@@ -39,13 +42,14 @@ def complex():
         img_zeros = item["img"]
         combined_zeros.append((real_zeros+img_zeros*1j))
 
+    finalFilterZeros =  combined_zeros + appliedAPFZeros
+    finalFilterPoles = combined_poles + appliedAPFPoles
+
     freq, complex_gain = signal.freqz_zpk(combined_zeros, combined_poles, 1)
     mag_gain = 20 * np.log10(abs(complex_gain))
-    if len(allpass)== 0 :
-        phase_gain = np.unwrap(np.angle(complex_gain))
-    else:
-        freq, complex_gain = signal.freqz_zpk(allPassZeros, allPassPoles, 1)
-        phase_gain = np.unwrap(np.angle(complex_gain))
+
+    _ , complex_gain = signal.freqz_zpk(finalFilterZeros, finalFilterPoles, 1)
+    phase_gain = np.unwrap(np.angle(complex_gain))
 
 #   sending response to front-end to plot
     return json.dumps({"freq": freq.tolist(), "mag": mag_gain.tolist(), "phase": phase_gain.tolist()})
@@ -62,16 +66,16 @@ def csv():
     y_axis = y_axis.to_numpy()
 
     filter_order = max(len(combined_poles), len(combined_zeros))
-    print(filter_order)
     #   To save calculations
     if (filter_order < 1):
         return jsonify({"x_axis": x_axis.tolist(), "y_axis": y_axis.tolist(), "filterd_signal": y_axis.tolist()})
-    if (len(allpass)==0):
-        num, dem = signal.zpk2tf(combined_zeros, combined_poles, 1)
-        filterd_signal = signal.lfilter(num, dem, y_axis).real
-    else:
-        num, dem = signal.zpk2tf(allPassZeros, allPassPoles, 1)
-        filterd_signal = signal.lfilter(num, dem, y_axis).real
+
+    finalFilterZeros =  combined_zeros + appliedAPFZeros
+    finalFilterPoles = combined_poles + appliedAPFPoles
+
+    num, dem = signal.zpk2tf(finalFilterZeros, finalFilterPoles, 1)
+    filterd_signal = signal.lfilter(num, dem, y_axis).real
+
     return jsonify({"x_axis": x_axis.tolist(), "y_axis": y_axis.tolist(), "filterd_signal": filterd_signal.tolist()})
 
 
@@ -88,19 +92,32 @@ def generated():
 #   Cut the signal to save memory
     if len(input_signal) > 2 * filter_order and len(input_signal) > 50  :
         del input_signal[0:filter_order]
-    if (len(allpass)==0):
-        num, dem = signal.zpk2tf(combined_zeros, combined_poles, 1)
-        output_signal = signal.lfilter(num, dem, input_signal).real
-    else:
-        num, dem = signal.zpk2tf(allPassZeros, allPassPoles, 1)
-        output_signal = signal.lfilter(num, dem, input_signal).real
+
+    finalFilterZeros =  combined_zeros + appliedAPFZeros
+    finalFilterPoles = combined_poles + appliedAPFPoles
+
+    num, dem = signal.zpk2tf(finalFilterZeros, finalFilterPoles, 1)
+    output_signal = signal.lfilter(num, dem, input_signal).real
+
     output_point = output_signal[-1]
     return json.dumps({"y_point": output_point})
 
 
 @app.route('/finalPhaseResponse', methods=['GET', 'POST'])
 def finalPhaseResponse():
-    freq, complex_gain = signal.freqz_zpk(allPassZeros, allPassPoles, 1)
+
+    appliedAPFZeros.clear()
+    appliedAPFPoles.clear()
+    
+    for zero in allPassZeros:
+        appliedAPFZeros.append(zero)
+    for pole in allPassPoles:
+        appliedAPFPoles.append(pole)
+
+    finalFilterZeros =  combined_zeros + appliedAPFZeros
+    finalFilterPoles = combined_poles + appliedAPFPoles
+
+    freq, complex_gain = signal.freqz_zpk(finalFilterZeros, finalFilterPoles, 1)
     result_phase = np.unwrap(np.angle(complex_gain))
 
     return jsonify({"result_phase":result_phase.tolist() , "freq":freq.tolist()})
@@ -108,33 +125,22 @@ def finalPhaseResponse():
 
 @app.route('/allPassPhase', methods=['GET', 'POST'])
 def allpassPhase():
-    allpass.clear()
-    allpassConj.clear()
     allPassZeros.clear()
     allPassPoles.clear()
+
     data = request.get_json()
     for item in data:
         if item["real"] == '':
             item["real"] = 0
         if item["img"] == '':
             item["img"] = 0
-
         real_poles = float(item["real"])
         img_poles = float(item["img"])
+        pole = real_poles+img_poles*1j
+        allPassZeros.append(1/np.conj(pole))
+        allPassPoles.append(pole)
 
-        print(real_poles, img_poles)
-
-        allpass.append(real_poles+img_poles*1j)
-        for i in combined_zeros:
-            allPassZeros.append(i)
-        for i in allpass:
-            allpassConj.append(1/np.conj(i))
-            allPassZeros.append(1/np.conj(i))
-            allPassPoles.append(i)
-        for i in combined_poles:
-            allPassPoles.append(i)
-
-    freq, complex_gain = signal.freqz_zpk(allpassConj, allpass, 1)
+    freq, complex_gain = signal.freqz_zpk(allPassZeros, allPassPoles, 1)
     Ap_phase = np.unwrap(np.angle(complex_gain))
     return jsonify({"Ap_phase":Ap_phase.tolist() , "freq":freq.tolist()})
 
